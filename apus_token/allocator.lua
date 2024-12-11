f6b0c6dbd658c89dceae060728ecf7170ea08d43
Ben Li <ben@Bens-iMac.local>
2024-12-11 20:05:18 +0800

    Remove assigning left to the first account

--- apus_token/allocator.lua
+++ apus_token/allocator.lua
@@ -38,6 +38,7 @@ function Allocator:compute(deposits, reward)
         return
     end
 
+    -- If there are remaining rewards, assign them to the first deposit
     if bint(left) > 0 then
         deposits[1].Reward = BintUtils.add(deposits[1].Reward, left)
     end
