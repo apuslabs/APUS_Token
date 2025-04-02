76bb99ca01917fd1c7fb705ffcb671d37d51c45f
Jason <contrltiger@gmai.comm>
2025-04-02 16:28:31 +0800

    Delete useless variables

--- apus_token/config.lua
+++ apus_token/config.lua
@@ -1,20 +1,9 @@
 -- [[
 --   AO Addresses
 -- ]]
--- Mint process address used for minting operations (dynamically set)
-AO_MINT_PROCESS = "1OEAToQGhSKV76oa1MFIGZ9bYxCJoxpXqtksApDdcu8"
--- APUS stats process address used for tracking statistics (dynamically set from runtime config)
-APUS_STATS_PROCESS = "3l3Qqm8YM0bDAeYv1OW3vLMat3GqEp2ieq4xYFyDtcU"
+
 -- The receiver address for the AO process, typically refers to the AO instance ID (dynamically set)
 AO_RECEIVER = "U-vRZXZP3tmczr8JOW_J1wqE1KFZo3YheKF5wYBcl1Y"
-
--- Minting cycle interval (in seconds)
--- Defines the cooldown period between minting cycles, set to 300 seconds (5 minutes)
-MINT_COOL_DOWN = 300
-
--- Start time for mint processing (initially set to a dynamic value if not defined)
--- Marks the moment when minting process begins
-StartMintTime = StartMintTime or 1734973200
 
 -- Log levels to control verbosity of logs
 -- Valid log levels: trace, debug, info, warn, error, fatal (default is 'info')
